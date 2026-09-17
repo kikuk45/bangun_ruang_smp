830d44187940a95b1e8a8ada8313e6fe7f137851
Rifqi Al Khadziq <m.rifqialkhadziq1313@gmail.com>
2026-09-17 07:14:44 +0700

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # --- KONFIGURASI HALAMAN ---
 st.set_page_config(
-    page_title="Modul Dimensi Tiga SMA", 
+    page_title="Modul Dimensi Tiga SMP", 
     page_icon="📐",
     layout="wide",
     initial_sidebar_state="expanded"
